cc56617ba5047d1ecfc914c7140c54ff7d5d05a3
Maxnxi <nisq@ya.ru>
2024-11-18 16:03:59 +0700

    Initial Commit

---
+++ some_VisionOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  some_VisionOS
+//
+//  Created by Maksim Ponomarev on 11/18/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ some_VisionOS/some_VisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  some_VisionOSApp.swift
+//  some_VisionOS
+//
+//  Created by Maksim Ponomarev on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct some_VisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
