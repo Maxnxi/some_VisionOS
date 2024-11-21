692cb7e3b388d4d5972165a156f9933df7dbb452
Maxnxi <nisq@ya.ru>
2024-11-21 22:21:09 +0700

    3rd commit

--- some_VisionOS/Part_2/View/Components/InventoryListItemView.swift
+++ some_VisionOS/Part_2/View/Components/InventoryListItemView.swift
@@ -5,8 +5,33 @@
 //  Created by Maksim Ponomarev on 11/20/24.
 //
 
+import RealityKit
 import SwiftUI
-import _RealityKit_SwiftUI
+
+struct InventoryListView: View {
+	
+	@StateObject var vm = InventoryListViewModel()
+	private let gridItems: [GridItem] = [.init(.adaptive(minimum: 240), spacing: 16)]
+	
+	var body: some View {
+		ScrollView {
+			LazyVGrid(columns: gridItems) {
+				ForEach(vm.items) { item in
+					InventoryListItemView(item: item)
+						.onDrag {
+							guard let usdzURL = item.usdzURL else { return NSItemProvider() }
+							return NSItemProvider(object: USDZItemProvider(usdzURL: usdzURL))
+						}
+				}
+			}
+			.padding(.vertical)
+			.padding(.horizontal, 30)
+			
+		}
+		.navigationTitle("XCA AR Inventory")
+		.onAppear { vm.listenToItems() }
+	}
+}
 
 struct InventoryListItemView: View {
 	
@@ -57,5 +82,10 @@ struct InventoryListItemView: View {
 }
 
 #Preview {
-	InventoryListItemView()
+	@StateObject var navVM = NavigationViewModel()
+	
+	return NavigationStack {
+		InventoryListView()
+			.environmentObject(navVM)
+	}
 }
